5efff60018c59786cd3bb2d6d9396dad593e9bc4
Mavludin <imevlud@gmail.com>
2021-10-24 23:06:36 +0300

    improved auth logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
@@ -23,17 +28,35 @@ export function App() {
 
         <main>
           <Switch>
-            <Route exact path="/" component={BlogPage} />
+            <Route
+              exact
+              path="/"
+              render={() => {
+                if (isLoggedIn) return <Redirect to="/blog" />;
+                return <Redirect to="/login" />;
+              }}
+            />
+
             <Route
               exact
               path="/login"
               render={(props) => (
+                !isLoggedIn ?
                 <LoginPage
                   {...props}
                   setIsLoggedIn={setIsLoggedIn}
                   setUserName={setUserName}
-                />
+                /> : <Redirect to="/blog" />
               )}
+            />
+
+            <Route
+              exact
+              path="/blog"
+              render={(props) => {
+                if (isLoggedIn) return <BlogPage {...props} />
+                return <Redirect to="/login" />
+              }}
             />
           </Switch>
         </main>
