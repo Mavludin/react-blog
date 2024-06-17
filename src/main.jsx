b0cf571035fab284a7c2c92912487c7217e3959f
Mavludin <imevlud@gmail.com>
2024-06-17 21:16:04 +0300

    add vite, typescript, update react

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import 'antd/dist/antd.css';
-import { App } from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { App } from './App'
+
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
 
@@ -15,17 +16,11 @@ const queryClient = new QueryClient({
   }
 });
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
